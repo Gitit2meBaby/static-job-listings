b32acd4c2ea8f352040ebec8e248792f87731226
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-23 18:50:58 +0700

    Tags now appending to the modal, data-tag attributes dont appear to be pushed onto the newly appended tags, havent started on the filtering function, also need to not allow for multiple tags of the same attribute to be appended to the modal, havent set clear button. Havent touched responsive design.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const app = document.querySelector('.app');
+    const modal = document.querySelector('.modal');
+    const filterTags = document.querySelector('.filter-tags');
 
-    const app = document.querySelector('.app')
-    const container = document.querySelector('.container')
+    app.addEventListener('click', (event) => {
+        if (event.target.classList.contains('tag')) {
+            const tagType = event.target.getAttribute('data-tag');
+            modal.classList.remove('hidden');
+            createAppendedTag(tagType);
+        }
+    });
 
     fetch('data.json')
         .then(response => response.json())
@@ -36,29 +44,67 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
 
         <div class="tags">
-          <div class="tag">Frontend</div>
-          <div class="tag">Senior</div>
+          <div class="tag" data-tag="${job.level}">${job.level}</div>
+          <div class="tag" data-tag="${job.role}">${job.role}</div>
         </div>
         `;
 
+
                 if (job.new == true) {
                     const newTag = jobListing.querySelector('.tag-new');
-                    newTag.classList.remove('hidden')
+                    newTag.classList.remove('hidden');
                 }
                 if (job.featured == true) {
                     const featuredTag = jobListing.querySelector('.tag-featured');
-                    featuredTag.classList.remove('hidden')
-                    jobListing.classList.add('featured')
+                    featuredTag.classList.remove('hidden');
+                    jobListing.classList.add('featured');
+                }
+
+                const tagsContainer = jobListing.querySelector('.tags');
+
+                if (job.languages.length > 0) {
+                    job.languages.forEach((language) => {
+                        createTag(tagsContainer, language);
+                    });
+                }
+                if (job.tools.length > 0) {
+                    job.tools.forEach((tool) => {
+                        createTag(tagsContainer, tool);
+                    });
                 }
 
                 app.appendChild(jobListing);
-            })
-
-
+            });
         })
-        .catch(error => console.error('Error fetching data:', error));
+        .catch((error) => console.error('Error fetching data:', error));
 
+    function createTag(container, text) {
+        const addTag = document.createElement('div');
+        addTag.classList.add('tag');
+        addTag.textContent = text;
+        addTag.setAttribute('data-tag', text);
+        container.appendChild(addTag);
+    }
 
+    function createAppendedTag(tagName) {
+        // Create an appended tag in the modal
+        const appendedTag = document.createElement('div');
+        appendedTag.classList.add('tag-appended');
+        appendedTag.innerHTML = appendedTagHTML(tagName);
+        filterTags.appendChild(appendedTag);
+    }
 
-
-})
+    function appendedTagHTML(tagName) {
+        return `
+    <div class="tag-title">
+        <p>${tagName}</p>
+    </div>
+    <div class="close-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+            <path fill="#FFF" fill-rule="evenodd"
+                d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z" />
+        </svg>
+    </div>
+`;
+    }
+});
