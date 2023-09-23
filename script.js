f9f273ee0e524b67e671934e352fd939db8dda57
Gitit2meBaby <thisworldofdans@gmail.com>
2023-09-23 22:04:51 +0700

    Basically a finished project, needs media queries for tablet, but does the job for the task required.

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const app = document.querySelector('.app');
     const modal = document.querySelector('.modal');
     const filterTags = document.querySelector('.filter-tags');
+    const clearBtn = document.querySelector('#clear')
 
+    // eventListener for tags to append to modal
     app.addEventListener('click', (event) => {
         if (event.target.classList.contains('tag')) {
             const tagType = event.target.getAttribute('data-tag');
             modal.classList.remove('hidden');
             createAppendedTag(tagType);
+            filterByTags();
         }
     });
 
@@ -49,19 +52,20 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
         `;
 
-
+                // add the NEW! div 
                 if (job.new == true) {
                     const newTag = jobListing.querySelector('.tag-new');
                     newTag.classList.remove('hidden');
                 }
+                //add the featured div, and featured border
                 if (job.featured == true) {
                     const featuredTag = jobListing.querySelector('.tag-featured');
                     featuredTag.classList.remove('hidden');
                     jobListing.classList.add('featured');
                 }
 
+                //iterate through the JSON file to find required tags
                 const tagsContainer = jobListing.querySelector('.tags');
-
                 if (job.languages.length > 0) {
                     job.languages.forEach((language) => {
                         createTag(tagsContainer, language);
@@ -78,6 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .catch((error) => console.error('Error fetching data:', error));
 
+    //create tags for each container (job listing)
     function createTag(container, text) {
         const addTag = document.createElement('div');
         addTag.classList.add('tag');
@@ -86,14 +91,26 @@ document.addEventListener('DOMContentLoaded', () => {
         container.appendChild(addTag);
     }
 
+    // Create an appended tag in the modal
     function createAppendedTag(tagName) {
-        // Create an appended tag in the modal
         const appendedTag = document.createElement('div');
         appendedTag.classList.add('tag-appended');
+        appendedTag.setAttribute('data-tag', tagName);
         appendedTag.innerHTML = appendedTagHTML(tagName);
         filterTags.appendChild(appendedTag);
+
+        const closeBtn = document.querySelectorAll('.close-btn')
+        closeBtn.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                appendedTag.remove();
+                filterByTags();
+                checkForEmptyModal();
+            });
+
+        })
     }
 
+    // appended tag (tags in modal) template
     function appendedTagHTML(tagName) {
         return `
     <div class="tag-title">
@@ -106,5 +123,62 @@ document.addEventListener('DOMContentLoaded', () => {
         </svg>
     </div>
 `;
+    }
+
+    // Store the selected tags in an array
+    let selectedTags = [];
+
+    // Filter the containers by data attribute
+    function filterByTags() {
+        selectedTags.length = 0;
+        const appendedTags = modal.querySelectorAll('.tag-appended');
+
+        appendedTags.forEach((appendedTag) => {
+            const tagType = appendedTag.getAttribute('data-tag');
+            selectedTags.push(tagType);
+        });
+
+        // Iterate through each job listing (container)
+        const jobListings = document.querySelectorAll('.container');
+        jobListings.forEach((jobListing) => {
+            const tags = jobListing.querySelectorAll('.tag');
+
+            const containsAllSelectedTags = selectedTags.every((selectedTag) => {
+                return Array.from(tags).some((tag) => tag.getAttribute('data-tag') === selectedTag);
+            });
+
+            if (!containsAllSelectedTags) {
+                jobListing.classList.add('hidden')
+            }
+            if (containsAllSelectedTags) {
+                jobListing.classList.remove('hidden')
+            }
+
+        });
+    }
+
+    // clear all tags from Modal
+    clearBtn.addEventListener('click', () => {
+        selectedTags = [];
+        filterByTags();
+        const jobListings = document.querySelectorAll('.container');
+        jobListings.forEach((jobListing) => {
+            jobListing.classList.remove('hidden');
+        });
+
+        const appendedTags = modal.querySelectorAll('.tag-appended');
+        appendedTags.forEach((appendedTag) => {
+            appendedTag.remove();
+        });
+
+        checkForEmptyModal()
+    });
+
+    function checkForEmptyModal() {
+        const filterTags = document.querySelector('.filter-tags');
+
+        if (filterTags.childElementCount === 0) {
+            modal.classList.add('hidden')
+        }
     }
 });
